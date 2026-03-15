3bbe8846fb6bcb7d1a02ebe7fdf53a760496764f
William Perron <wil.perron@gmail.com>
2026-03-15 15:33:20 -0400

    ajout des dépenses et des salaires

--- R/data/fiscal_template.R
+++ R/data/fiscal_template.R
@@ -1,10 +1,23 @@
-MAX_AGE <- 100
+MAX_AGE <- 100 # en date du 1er janvier
 inflation <- 1.0275
 ipc <- 1.02
 rendement_brut <- 1.07
 RENDEMENT <- rendement_brut / inflation
-START_AGE <- 20
+START_AGE <- 20 # en date du 1er janvier
 
+# en date du 1er janvier
 actifs <- list(
   cash = 400000
+)
+
+# en date du 1er janvier
+revenus <- data.frame(
+  age = START_AGE:MAX_AGE,
+  revenu_emploi = 0
+)
+
+# en date du 1er janvier
+depenses <- data.frame(
+  age = START_AGE:MAX_AGE,
+  depenses = 40000
 )
