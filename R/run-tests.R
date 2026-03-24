791fb78eed4ccfae7f5d1de7e17b508ebc09f3c8
William Perron <wil.perron@gmail.com>
2026-03-23 20:12:02 -0400

    test intégration

--- R/run-tests.R
+++ R/run-tests.R
@@ -1,7 +1,18 @@
 library(mockery)
 library(testthat)
 
-test_files <- list.files("R/tests", "test.*\\.R", recursive = TRUE, full.names = TRUE)
+### tests unitaires
+test_files <- list.files("R/tests/unit", "test.*\\.R", recursive = TRUE, full.names = TRUE)
+
+test_results <- sapply(test_files, source)["value", ]
+test_true <- sapply(test_results, isTRUE)
+print(test_true)
+
+# lancer l'erreur si les tests ne passent pas
+expect_true(all(test_true))
+
+### test d'intégration
+test_files <- list.files("R/tests/integration", "test.*\\.R", recursive = TRUE, full.names = TRUE)
 
 test_results <- sapply(test_files, source)["value", ]
 test_true <- sapply(test_results, isTRUE)
