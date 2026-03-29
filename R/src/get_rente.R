4603b01c3138db06533a5c36d9f19c7ab4fa4712
William Perron <wil.perron@gmail.com>
2026-03-29 11:30:33 -0400

    linting

--- R/src/get_rente.R
+++ R/src/get_rente.R
@@ -1,13 +1,13 @@
 # rente d'employeur
 get_rente <- function(age, salaires, annees_travaillees, ipc) {
   # nolint https://www.canada.ca/fr/agence-revenu/services/impot/administrateurs-regimes-enregistres/fesp/plafonds-cd-reer-rpdb-celi-mgap.html
-  plafond_annuel_pd_facteur_equiv <- 3756.67
+  plafond_annuel_pd_fact_equiv <- 3756.67
   mga <- 71300
 
   mean_5_salaires <- mean(tail(sort(salaires), 5))
   (age >= 65) * (
     annees_travaillees * pmin(
-      plafond_annuel_pd_facteur_equiv,
+      plafond_annuel_pd_fact_equiv,
       0.015 * pmin(mga, mean_5_salaires) +
         0.02 * pmax(0, mean_5_salaires - mga)
     )
