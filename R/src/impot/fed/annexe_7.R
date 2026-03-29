c480160847c933dec72f04be6cfab16eccabc673
William Perron <wil.perron@gmail.com>
2026-03-29 11:00:49 -0400

    linting

--- R/src/impot/fed/annexe_7.R
+++ R/src/impot/fed/annexe_7.R
@@ -9,11 +9,11 @@ annexe_7 <- function(cotisations_inutilisees_versees_dispo_a_deduire,
   l24500 <- l2 <- pmax(0, cotisations_versees_reer)
 
   l5 <- l1 + l24500
-  
+
   # partie B
   # pour RAP
   l10 <- l6 <- l5
-  
+
   # partie C
   # déduction pour REER
   l11 <- max_deductible_reer
@@ -21,10 +21,10 @@ annexe_7 <- function(cotisations_inutilisees_versees_dispo_a_deduire,
   l18 <- deduction_reer
   stopifnot(l18 <= l17)
   l20800 <- l20 <- pmin(l10, l18)
-  
+
   # partie D
   # cotisations inutilisées versées à un REER disponibles à reporter
   cotis_inutil_vers_disp_deduc <- l10 - l20
-  
+
   list(l20800 = l20800, l24500 = l24500, cotis_inutil_vers_disp_deduc = cotis_inutil_vers_disp_deduc)
 }
--- R/src/try_strategy.R
+++ R/src/try_strategy.R
@@ -33,7 +33,7 @@ try_stategy <- function(actifs, revenus, depenses, strategy) {
       "attention, droits de cotisations au reer dépassés"
     )
     # car FERR
-    if (start_age + i - 1 >= 71 & strategy[i, "NET_COTIS_REER"] > 0) stop("Pas le droit de cotiser au REER, car FERR")
+    if (start_age + i - 1 >= 71 && strategy[i, "NET_COTIS_REER"] > 0) stop("Pas le droit de cotiser au REER, car FERR")
     if (-strategy[i, "NET_COTIS_REER"] < (retrait_min_ferr(start_age + i - 1) * actifs$reer$current_value)) stop(
       "Retraits du REER insuffisants car FERR"
     )
