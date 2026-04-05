bfa879a92b87cf1117d3a6ae4f0282ec17a245bd
William Perron <wil.perron@gmail.com>
2026-04-05 09:00:48 -0400

    lintr

--- R/src/optim/optim_flat_expenses.R
+++ R/src/optim/optim_flat_expenses.R
@@ -3,29 +3,29 @@ source("R/src/try_strategy.R")
 # according to a specific strategy of investments/withdrawal/work/retirement, how much can i spend each year
 # (flat except for inflation) to fulfill my needs?
 optim_flat_expenses <- function(data_filepath, inflation_over_ipc, eps = 0.01) {
-  
+
   # in today's dollars
   # estimation of propabe yearly_expenses, very simplified
   source(data_filepath)
   yearly_expenses <- actifs$cash + actifs$nonenr_capital + actifs$nonenr_gain + actifs$celi$current_value
   yearly_expenses <- yearly_expenses * (rendement_brut / ipc - 1)
-  
+
   # bounds on the yearly_expenses amount
   # upper will be set after first failure
   bounds <- c(0, NA)
 
   repeat {
-    
+
     message(c("[", round(bounds[1], 2), ", ", round(bounds[2], 2), "]"))
     # reinitiate assets
     source(data_filepath)
-    
+
     # set expenses level
     depenses <- data.frame(
       age = start_age:max_age,
       depenses = yearly_expenses * (inflation_over_ipc / ipc)^(0:(max_age - start_age))
     )
-    
+
     # try to live while spending `depenses` schedule
     bounds <- tryCatch({
       try_strategy(actifs, revenus, depenses, strategy)
