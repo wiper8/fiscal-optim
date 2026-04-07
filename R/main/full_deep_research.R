636d15cb7a0aebe57e76483fcba0d3ae4c7e70ff
William Perron <wil.perron@gmail.com>
2026-04-07 19:35:53 -0400

    ajout fichier full_deep_research.R

--- R/main/full_deep_research.R
+++ R/main/full_deep_research.R
@@ -13,7 +13,7 @@ data_filepath <- if (file.exists(private_filename)) {
 
 source(data_filepath)
 
-df <- work_years_to_expenses(data_filepath, salaire, limit_itr = 100,
+df <- work_years_to_expenses(data_filepath, salaire, limit_itr = 1000,
                              bloc_splits = c(30, 35, 40, 45, 50, 55, 65, 71, 75, 80, 85, 90, 95))
 
 plot_work_years_expenses(df)
