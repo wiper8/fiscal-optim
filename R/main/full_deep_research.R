65f8dcd92d154af6abe6aef4e6d7be6dfa97c02c
William Perron <wil.perron@gmail.com>
2026-04-27 07:39:45 -0400

    full epp research a 2 optimisations différentes

--- R/main/full_deep_research.R
+++ R/main/full_deep_research.R
@@ -13,6 +13,28 @@ data_filepath <- if (file.exists(private_filename)) {
 
 source(data_filepath)
 
+age_retraite <- revenus$age[head(which(revenus$revenu_emploi == 0), 1)]
+
+tmp <- guided_maximise_expenses(start_age, max_age,
+                                bloc_splits = c(35, 45, 55, 65, 71, 75, 80, 85, 90, 95),
+                                data_filepath = data_filepath,
+                                eps = 10, inflation = inflation, ipc = ipc, verbose = 4,
+                                limit_time = 120,
+                                optimiser = "swarm"
+)
+
+actifs_hist <- try_strategy(
+  actifs, revenus, get_expenses_ipc(start_age, max_age, tmp$depenses + depenses_variables$depenses, inflation, ipc),
+  tmp$strategy, passed_revenus,
+  start_age, max_age
+)
+
+key_moments <- c(start_age, age_retraite, 65, 71, 75, max_age)
+
+plot_actifs_hist(actifs_hist, key_moments)
+
+###
+
 df2 <- work_years_to_expenses(data_filepath, salaire,
   limit_time = 120,
   optimiser = "swarm",
